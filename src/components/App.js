4a3ea3907682968ed66d285126be6c21aedcacdf
Shubham Agrawal <mail2shubham001@gmail.com>
2016-12-11 20:20:43 +0530

    header links removed

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/registration">Demo App</Link>
-        {' | '}
-        <br/>
         {this.props.children}
       </div>
     );
