d4af0bbffb679957774ef40d35c5044992887f15
Anand Mani <cma.camelot@gmail.com>
2016-12-26 20:02:36 +0530

    Added prod server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static('dist'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist', 'index.html'))
+})
+
+var port = 80;
+app.listen(port);
