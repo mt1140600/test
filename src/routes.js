d379b1e9c8accc02ebd6d405469105e51c1ea336
anandmani <cma.camelot@gmail.com>
2017-02-07 12:25:19 +0530

    re-enabled auth routing

--- src/routes.js
+++ src/routes.js
@@ -12,25 +12,25 @@ import Verification from './views/Verification';
 import VerifyEmail from './views/VerifyEmail';
 
 export default (
-  // <Route path="/" component={App}>
-  //   <IndexRoute component={UserIsNotAuthenticated(Login)}/>
-  //   <Route path="signup" component={UserIsNotAuthenticated(Signup)} />
-  //   <Route path="verifyEmail" component={VerifyEmail} />
-  //   <Route path="reset" component={UserIsNotAuthenticated(ResetPassword)} />
-  //   <Route path="reset2" component={UserIsNotAuthenticated(ResetPassword2)} />
-  //   <Route path="registration" component={UserIsAuthenticated(UserIsEmailVerified(AccountSetup))}/>
-  //   <Route path="verification" component={UserIsAuthenticated(UserIsEmailVerified(Verification))} />
-  //   <Route path="dashboard" component={UserIsAuthenticated(UserIsEmailVerified(ProductUpload))} />
-  // </Route>
-
   <Route path="/" component={App}>
-    <IndexRoute component={Login}/>
-    <Route path="signup" component={Signup} />
+    <IndexRoute component={UserIsNotAuthenticated(Login)}/>
+    <Route path="signup" component={UserIsNotAuthenticated(Signup)} />
     <Route path="verifyEmail" component={VerifyEmail} />
-    <Route path="reset" component={ResetPassword} />
-    <Route path="registration" component={AccountSetup}/>
-    <Route path="verification" component={Verification} />
-    <Route path="dashboard" component={ProductUpload} />
-    <Route path="reset2" component={ResetPassword2} />
+    <Route path="reset" component={UserIsNotAuthenticated(ResetPassword)} />
+    <Route path="reset2" component={UserIsNotAuthenticated(ResetPassword2)} />
+    <Route path="registration" component={UserIsAuthenticated(UserIsEmailVerified(AccountSetup))}/>
+    <Route path="verification" component={UserIsAuthenticated(UserIsEmailVerified(Verification))} />
+    <Route path="dashboard" component={UserIsAuthenticated(UserIsEmailVerified(ProductUpload))} />
   </Route>
+
+  // <Route path="/" component={App}>
+  //   <IndexRoute component={Login}/>
+  //   <Route path="signup" component={Signup} />
+  //   <Route path="verifyEmail" component={VerifyEmail} />
+  //   <Route path="reset" component={ResetPassword} />
+  //   <Route path="registration" component={AccountSetup}/>
+  //   <Route path="verification" component={Verification} />
+  //   <Route path="dashboard" component={ProductUpload} />
+  //   <Route path="reset2" component={ResetPassword2} />
+  // </Route>
 );
