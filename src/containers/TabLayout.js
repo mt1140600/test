37c0c1dad2f1f51f74b7ebee10172f0b4e50ec83
mt1140600 <mt1140600@iitd.ac.in>
2017-04-06 10:00:01 +0530

    Slide Animations from 1 side

--- src/containers/TabLayout.js
+++ src/containers/TabLayout.js
@@ -16,12 +16,10 @@ require('velocity-animate');
 require('velocity-animate/velocity.ui');
 var effects1 = ['slideLeft','slideRight', 'fade'];
 var VelocityComponent = require('../../velocity-component');
-import VelocityTransitionGroup from 'velocity-transition-group';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 const tabs      = ["Mobile Verification", "Seller Information",  "Tax Details",  "Payment Details",  "Point of Contact", "Additional Information", "Terms & Conditions"];
 const tabPanels = [     VerifyOtp,            SellerInfo,          TaxDetails,    PaymentDetails,       POCDetails,            AddInfo,                    TnC];
-
 
 class TabLayout extends Component {
   constructor() {
@@ -47,21 +45,32 @@ class TabLayout extends Component {
     else
       var animation = 'transition.'+effects1[1] + (this.isIn ? 'In' : 'Out');
     
-    
     let DynamicTabPanel = item; //Not using item directly as JSX requires First letter to be capitalised
-    
     return(
-      <TabPanel key={index} >
-      <VelocityComponent key = {this.effects1} animation={animation} runOnMount={false} duration = {2000} >
-      <TabPanel key={index} >
-        <DynamicTabPanel/>
-      </TabPanel> 
-      </VelocityComponent>
+      <TabPanel key={index}> 
+           <ReactCSSTransitionGroup
+          transitionName="slide"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}>
+
+            <DynamicTabPanel/>
+          
+          </ReactCSSTransitionGroup>        
       </TabPanel>
     );
   }
-
+  showTabPanels(){
+    document.getElementsByClassName("pt-tab-panel").style.display = "block";
+  }
   componentWillMount(){
+    const script = document.createElement("script");
+
+    script.src = "https://fb.me/react-with-addons-0.13.3.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
     // setTimeout(this.props.actionTabChange.bind(null,0),100); //hack to fix tab line. Not required now. Problem was arising because we were requiring css in index.js.  Now we are linking css from html. The Tab component requires css to be loaded prior to it for proper functioning
     this.props.loadForm();
   }
@@ -73,11 +82,12 @@ class TabLayout extends Component {
 
   render() {
     return(           
+      
         <Tabs className="tabs1" selectedTabIndex={this.props.currentTab} onChange={this.handleTabChange}>
           <TabList className="pt-large" style={{padding: "0px 20px 0px 20px"}}>
             {tabs.map(this.renderTabs)}
           </TabList>          
-            {tabPanels.map(this.renderTabPanels)}          
+            {tabPanels.map(this.renderTabPanels)} 
         </Tabs>  
                  
     );
